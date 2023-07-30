85739444c10ec2fcac27dd61263c396d7b97057e
ankit <ankit.gupta@kreeti.com>
2023-07-30 10:14:51 +0530

    new assign2

--- assignment2/calculator.js
+++ assignment2/calculator.js
@@ -1,2 +1,17 @@
 //make an even biased calculator
 
+function result(num1,num2,opp){
+
+    switch(opp){
+        case 1: console.log(num1+num2);
+                break;
+        case 2: console.log(num1-num2);
+                break;
+        case 3: console.log(num1*num2);
+                break;
+        case 4: console.log(num1/num2);
+                break;
+        default: console.log("!! Invalid Operator !!");
+    }
+}
+
