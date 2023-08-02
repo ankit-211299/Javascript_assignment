b1bd94151fdce04edece2c9d6c2a28fab99e0ef9
ankit <ankit.gupta@kreeti.com>
2023-08-02 15:06:51 +0530

    assignment updated

--- assignment3/DOM_assign/Calculate_Area/script.js
+++ assignment3/DOM_assign/Calculate_Area/script.js
@@ -2,9 +2,22 @@
 // the user inputs the width and height into two input fields and clicks a
 // button.
 
-const len = parseInt(prompt("Length : "));
-console.log(len);
+const onSubmit = () => {
 
-const br = parseInt(prompt("Breadth : "));
-console.log(br);
+    console.log("Button Pressed");
+    const lenVal = parseInt(document.getElementById("len").value);
+    console.log(lenVal);
+    const breVal = parseInt(document.getElementById("bre").value);
+    console.log(breVal);
+    const result = document.getElementById("res");
 
+    (function (num1,num2) {
+        const sum = num1+num2;
+        console.log(sum);
+        result.innerHTML = "Results ="+" "+sum;
+    })(lenVal,breVal);
+};
+
+const submitButton = document.getElementById("sub");
+
+submitButton.addEventListener("click",onSubmit);
