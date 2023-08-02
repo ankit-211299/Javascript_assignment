12db54d374efc17cce0a634627faf447e77baa01
ankit <ankit.gupta@kreeti.com>
2023-08-02 13:49:43 +0530

    update

---
+++ assignment3/DOM_assign/Calculate_Area/readme.txt
@@ -0,0 +1,3 @@
+Write a JavaScript program that calculates the area of a rectangle when
+the user inputs the width and height into two input fields and clicks a
+button.
---
+++ assignment3/DOM_assign/Calculate_Area/script.js
@@ -0,0 +1,10 @@
+// Write a JavaScript program that calculates the area of a rectangle when
+// the user inputs the width and height into two input fields and clicks a
+// button.
+
+const len = parseInt(prompt("Length : "));
+console.log(len);
+
+const br = parseInt(prompt("Breadth : "));
+console.log(br);
+
