1470de4887b4c7a0460353c9ff8b77d1a201ff1c
ankit <ankit.gupta@kreeti.com>
2023-08-03 14:52:07 +0530

    updated assignment

--- assignment3/assign3.2.js
+++ assignment3/assign3.2.js
@@ -4,13 +4,27 @@
 
 
 let arr = [1, -4, 12, 0, -3, 29, -150]
-let sum=0;
 
-for (var n of arr) {
-    if (n > 0) {
-        sum += n;
-    }
-}
-console.log(sum);
+// let sum=0;
+
+// for (var n of arr) {
+//     if (n > 0) {
+//         sum += n;
+//     }
+// }
+// console.log(sum);
 
 // TODO: Using reduce(), filter()
+
+const ans = arr.reduce((total,value) => value>0 ? total+value : total, 0);
+
+// const ans = arr.reduce((total,value) => {
+//     if(value>0){
+//         return value+total;
+//     }
+//     else{
+//         return total;
+//     }
+// },0);
+
+console.log(ans);
--- assignment3/assign3.3.js
+++ assignment3/assign3.3.js
@@ -3,10 +3,15 @@
 
 
 let arr1 = [1,2,3,4,5];
-let arr2=[];
-for(let n of arr1){
-    arr2.push(Math.pow(n,2));
-}
-console.log(arr2);
+
+
+// let arr2=[];
+// for(let n of arr1){
+//     arr2.push(Math.pow(n,2));
+// }
+// console.log(arr2);
 
 // TODO: Using map()
+
+const arr3 = arr1.map((value) => value*value);
+console.log(arr3);
