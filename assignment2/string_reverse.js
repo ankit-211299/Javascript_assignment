6d57f8ae7a58555916068d5a0eec91fe1f32f578
ankit <ankit.gupta@kreeti.com>
2023-08-03 12:01:46 +0530

    Assignment Update

--- assignment2/string_reverse.js
+++ assignment2/string_reverse.js
@@ -1,29 +1,38 @@
-let fname = "Ankit Gupta ";
-let word = "",
-    neww = "";
+// let fname = "Ankit Gupta ";
+// let word = "",
+//     neww = "";
 
-for (var i = 0; i < fname.length; i++) {
-    // console.log(fname[i]);
-    if (fname[i] != " ") {
-        word = word + fname[i];
-    } else {
-        console.log(reverse(word));
-        neww = neww + reverse(word) + " ";
-        word = "";
-    }
-}
+// for (var i = 0; i < fname.length; i++) {
+//     // console.log(fname[i]);
+//     if (fname[i] != " ") {
+//         word = word + fname[i];
+//     } else {
+//         console.log(reverse(word));
+//         neww = neww + reverse(word) + " ";
+//         word = "";
+//     }
+// }
 
-function reverse(word) {
-    // console.log(word);
-    var rev = "";
+// function reverse(word) {
+//     // console.log(word);
+//     var rev = "";
 
-    for (var i = word.length - 1; i >= 0; i--) {
-        // console.log(word[i]);
-        rev = rev + word[i];
-    }
-    return rev;
-}
+//     for (var i = word.length - 1; i >= 0; i--) {
+//         // console.log(word[i]);
+//         rev = rev + word[i];
+//     }
+//     return rev;
+// }
 
-console.log(neww);
+// console.log(neww);
 
 // TODO: Do this with split()
+
+let n = "Ankit Gupta"
+
+let words = n.split(" ");
+
+words.forEach((nam) => {
+    var str = nam.split("");
+    console.log(str.reverse().join(""));
+});
