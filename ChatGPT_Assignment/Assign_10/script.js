fbf4ab6d7186f28f2b0ca77cb68fec04dac5375f
ankit <ankit.gupta@kreeti.com>
2023-10-29 20:17:31 +0530

    updated

--- ChatGPT_Assignment/Assign_10/script.js
+++ ChatGPT_Assignment/Assign_10/script.js
@@ -1,8 +1,15 @@
-const imageElements = document.querySelectorAll(".img");
+const imgElements = document.querySelectorAll(".img");
+const fullSizeImageContainer = document.getElementById("fullSizeImageContainer");
+const fullSizeImage = document.getElementById("fullSizeImage");
 
-imageElements.forEach((imageElement)=>{
-    imageElement.addEventListener("click",(event) => {
-        const imageSource = event.target.src;
-        console.log("Clicked on:"+imageSource);
+imgElements.forEach(imgElement => {
+    imgElement.addEventListener("click", function(event) {
+        const elementName = imgElement.getAttribute("src");
+        fullSizeImage.src = elementName;
+        fullSizeImageContainer.style.display = "block";
     });
+});
+
+fullSizeImageContainer.addEventListener("click", function() {
+    fullSizeImageContainer.style.display = "none";
 });
