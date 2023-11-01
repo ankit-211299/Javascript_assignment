9021ac56f8118ec4f964ff60ec44c2dd7cfa87eb
ankit <ankit.gupta@kreeti.com>
2023-11-01 13:04:54 +0530

    updated assignment

--- ChatGPT_Assignment/Assign_10/script.js
+++ ChatGPT_Assignment/Assign_10/script.js
@@ -1,21 +1,31 @@
 const imgElements = document.querySelectorAll(".img");
 const fullScaleImage = document.getElementById("fullScaleImage");
-const fullScaleImageContainer = document.getElementById("fullScaleImageContainer");
+const fullScaleImageContainer = document.getElementById(
+  "fullScaleImageContainer"
+);
 const closeContainer = document.getElementById("closeContainer");
 const closeButton = document.getElementById("close");
 
-imgElements.forEach((imgElement)=>{
-    imgElement.addEventListener("click",function(event){
-        const imgSource = imgElement.getAttribute("src");
-        fullScaleImage.src = imgSource;
-        closeContainer.style.display = "flex";
-        fullScaleImageContainer.style.display = "flex";
-        document.body.style.overflow = "hidden";
-    });
+imgElements.forEach((imgElement) => {
+  imgElement.addEventListener("click", function (event) {
+    const imgSource = imgElement.getAttribute("src");
+    fullScaleImage.src = imgSource;
+    openModal();
+  });
 });
 
-closeButton.addEventListener("click",function(event){
-    closeContainer.style.display = "none";
-    fullScaleImageContainer.style.display = "none";
-    document.body.style.overflow = "auto";
+closeButton.addEventListener("click", function (event) {
+  closeModal();
 });
+
+function openModal() {
+  closeContainer.style.display = "flex";
+  fullScaleImageContainer.style.display = "flex";
+  document.body.style.overflow = "hidden";
+}
+
+function closeModal() {
+  closeContainer.style.display = "none";
+  fullScaleImageContainer.style.display = "none";
+  document.body.style.overflow = "auto";
+}
