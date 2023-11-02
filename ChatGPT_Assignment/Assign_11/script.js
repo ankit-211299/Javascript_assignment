e14307e2cedb2504554ffee5c1d89fbf3fdc2bed
ankit <ankit.gupta@kreeti.com>
2023-11-02 11:40:17 +0530

    updated

--- ChatGPT_Assignment/Assign_11/script.js
+++ ChatGPT_Assignment/Assign_11/script.js
@@ -23,12 +23,19 @@ function editTable(name,age){
     const trElement = document.createElement("tr");
     const tdNameElement = document.createElement("td");
     const tdAgeElement = document.createElement("td");
+    const thCheckBox = document.createElement("th");
+    const new_checkBox = document.createElement("input");
+    
+    new_checkBox.type = "checkbox";
+    new_checkBox.className = "check_box";
+    thCheckBox.appendChild("new_checkbox");
 
     tdNameElement.innerHTML = name;
     tdAgeElement.innerHTML = age;
 
     trElement.appendChild(tdNameElement);
     trElement.appendChild(tdAgeElement);
+    trElement.appendChild(thCheckBox);
 
     tableElement.appendChild(trElement);
 }
@@ -47,4 +54,14 @@ selectAllElement.addEventListener("change",function(event){
             checkBoxElement.checked = false;
         });
     }
+});
+
+//deletion
+const deleteButtonElement = document.getElementById("deleteButton");
+deleteButtonElement.addEventListener("click",function(){
+    checkBoxElements.forEach((checkBoxElement)=>{
+        if(checkBoxElement.checked === true){
+            checkBoxElement.parentElement.parentElement.value = "";
+        }
+    });
 });
